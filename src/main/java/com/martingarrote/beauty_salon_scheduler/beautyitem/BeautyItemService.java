a610428d3c5cb6c48ee61389c315119c9d03c9e5
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-05 20:54:30 -0300

    implement create method in BeautyItemService

--- src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemService.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemService.java
@@ -1,6 +1,7 @@
 package com.martingarrote.beauty_salon_scheduler.beautyitem;
 
 
+import com.martingarrote.beauty_salon_scheduler.beautyitem.dto.BeautyItemDTO;
 import com.martingarrote.beauty_salon_scheduler.mapper.BeautyItemMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,4 +12,12 @@ public class BeautyItemService {
 
     private final BeautyItemRepository repository;
     private final BeautyItemMapper mapper;
+
+    public BeautyItemDTO create(BeautyItemDTO dto) {
+        BeautyItem entity = mapper.toEntity(dto);
+
+        BeautyItem savedEntity = repository.save(entity);
+
+        return mapper.toDTO(savedEntity);
+    }
 }
