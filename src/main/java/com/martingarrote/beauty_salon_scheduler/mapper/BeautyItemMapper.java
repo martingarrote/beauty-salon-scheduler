919fdcbd20a987c2493e4b7a4076342a8885cf6d
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-05 19:53:40 -0300

    implement toDTO mapper in BeautyItemMapper

--- src/main/java/com/martingarrote/beauty_salon_scheduler/mapper/BeautyItemMapper.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/mapper/BeautyItemMapper.java
@@ -1,9 +1,13 @@
 package com.martingarrote.beauty_salon_scheduler.mapper;
 
+import com.martingarrote.beauty_salon_scheduler.beautyitem.BeautyItem;
+import com.martingarrote.beauty_salon_scheduler.beautyitem.dto.BeautyItemDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface BeautyItemMapper {
     BeautyItemMapper INSTANCE = Mappers.getMapper(BeautyItemMapper.class);
+
+    BeautyItemDTO toDTO(BeautyItem beautyItem);
 }
