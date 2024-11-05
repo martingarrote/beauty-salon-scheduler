5d0ada41f569f51d74d3c5d5bab2faa26e7b6b52
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-05 15:43:06 -0300

    first commit

---
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/BeautySalonSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.martingarrote.beauty_salon_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautySalonSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeautySalonSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martingarrote/beauty_salon_scheduler/BeautySalonSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martingarrote.beauty_salon_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeautySalonSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
