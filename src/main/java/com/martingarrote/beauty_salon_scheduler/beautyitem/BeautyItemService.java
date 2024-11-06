2e4f1f48c764c6e1c0975df41cee5b487b949f07
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-05 21:14:58 -0300

    implement search method with dynamic filters in service layer

--- src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemService.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemService.java
@@ -3,7 +3,11 @@ package com.martingarrote.beauty_salon_scheduler.beautyitem;
 
 import com.martingarrote.beauty_salon_scheduler.beautyitem.dto.BeautyItemDTO;
 import com.martingarrote.beauty_salon_scheduler.mapper.BeautyItemMapper;
+import com.martingarrote.beauty_salon_scheduler.mapper.common.PageDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -27,5 +31,18 @@ public class BeautyItemService {
         List<BeautyItem> dtoList = repository.findAll();
 
         return dtoList.stream().map(mapper::toDTO).toList();
+    }
+
+    public PageDTO<BeautyItemDTO> search(String name, int duration, double price, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<BeautyItem> result = repository.search(name, duration, price, pageable);
+
+        return new PageDTO<>(
+                result.getContent().stream().map(mapper::toDTO).toList(),
+                result.getNumber(),
+                result.getSize(),
+                result.getTotalElements(),
+                result.getTotalPages()
+        );
     }
 }
