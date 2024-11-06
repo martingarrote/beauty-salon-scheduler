27b85b8cb19e81e1a3f753cc99b0feb92cab855e
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-06 12:17:32 -0300

    implement create endpoint in BeautyItemController

--- src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemController.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/beautyitem/BeautyItemController.java
@@ -1,14 +1,23 @@
 package com.martingarrote.beauty_salon_scheduler.beautyitem;
 
+import com.martingarrote.beauty_salon_scheduler.beautyitem.dto.BeautyItemDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping(value = "/api/beauty-items")
 @RequiredArgsConstructor
 public class BeautyItemController {
 
     private final BeautyItemService service;
 
+    @PostMapping
+    public ResponseEntity<BeautyItemDTO> create(@RequestBody BeautyItemDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.create(dto));
+    }
 }
