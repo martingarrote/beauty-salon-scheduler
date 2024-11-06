8561fb9cdc5408db5406a4df5b5e9cf247909893
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-06 17:18:07 -0300

    add GlobalExceptionHandler

---
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.martingarrote.beauty_salon_scheduler.exceptions;
+
+import com.martingarrote.beauty_salon_scheduler.exceptions.beautyitem.BeautyItemNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice(annotations = RestController.class)
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+}
