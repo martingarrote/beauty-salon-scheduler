5acf8603c908f91928bf970238a28378227656e7
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-08 17:51:55 -0300

    improve exception message in BeautyItemNotFoundException

--- src/main/java/com/martingarrote/beauty_salon_scheduler/exceptions/beautyitem/BeautyItemNotFoundException.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/exceptions/beautyitem/BeautyItemNotFoundException.java
@@ -2,7 +2,7 @@ package com.martingarrote.beauty_salon_scheduler.exceptions.beautyitem;
 
 public class BeautyItemNotFoundException extends RuntimeException {
 
-    public BeautyItemNotFoundException() {super("Beauty item not found.");}
+    public BeautyItemNotFoundException() {super("The specified beauty item could not be found. Please check the item ID and try again.");}
     public BeautyItemNotFoundException(String message) {super(message);}
 
 }
