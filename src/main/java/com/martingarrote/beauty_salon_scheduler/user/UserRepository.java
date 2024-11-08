491b0dd9ea625c4e1153acd5ab43c90af4e36237
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-08 20:58:38 -0300

    implement method to find users by authority

--- src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserRepository.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserRepository.java
@@ -1,8 +1,10 @@
 package com.martingarrote.beauty_salon_scheduler.user;
 
+import com.martingarrote.beauty_salon_scheduler.authority.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -10,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+
+    List<User> findByAuthorities_Authority(String authority);
 }
