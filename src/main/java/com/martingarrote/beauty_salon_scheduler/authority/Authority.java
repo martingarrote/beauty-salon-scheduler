b2a9a21413831b0512f0930c8673127aa5eae9f1
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-08 17:12:52 -0300

    implement Authority entity

---
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/authority/Authority.java
@@ -0,0 +1,17 @@
+package com.martingarrote.beauty_salon_scheduler.authority;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "authorities")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Authority {
+
+    @Id
+    private String authority;
+}
