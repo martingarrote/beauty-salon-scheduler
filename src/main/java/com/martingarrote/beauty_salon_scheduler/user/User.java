0b04708b3de16e93dee730f2db6feb875d6ee22e
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-08 17:26:20 -0300

    implement User entity

---
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/user/User.java
@@ -0,0 +1,56 @@
+package com.martingarrote.beauty_salon_scheduler.user;
+
+import com.martingarrote.beauty_salon_scheduler.authority.Authority;
+import com.martingarrote.beauty_salon_scheduler.user.enums.FaceShape;
+import com.martingarrote.beauty_salon_scheduler.user.enums.HairCurl;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String name;
+
+    @NotBlank
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NotBlank
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private FaceShape faceShape;
+
+    @Enumerated(EnumType.STRING)
+    private HairCurl hairCurl;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "user_authority_mapping",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "authority"))
+    private Set<Authority> authorities;
+
+    public List<SimpleGrantedAuthority> mapAuthorityToSimpleGrantedAuthority() {
+        return authorities.stream().map(Authority::getAuthority).map(SimpleGrantedAuthority::new).toList();
+    }
+}
