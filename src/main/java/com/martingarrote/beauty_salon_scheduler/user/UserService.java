3fc75529e3b327ede00293973384bc68e1b5b475
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-09 17:09:48 -0300

    implement login method

--- src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserService.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserService.java
@@ -3,8 +3,12 @@ package com.martingarrote.beauty_salon_scheduler.user;
 import com.martingarrote.beauty_salon_scheduler.authority.Authority;
 import com.martingarrote.beauty_salon_scheduler.authority.AuthorityRepository;
 import com.martingarrote.beauty_salon_scheduler.exceptions.user.EmailAlreadyInUseException;
+import com.martingarrote.beauty_salon_scheduler.exceptions.user.EmailNotFoundException;
+import com.martingarrote.beauty_salon_scheduler.exceptions.user.WrongPasswordException;
 import com.martingarrote.beauty_salon_scheduler.security.TokenService;
+import com.martingarrote.beauty_salon_scheduler.user.dto.LoginDTO;
 import com.martingarrote.beauty_salon_scheduler.user.dto.SignupDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.TokenDTO;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,6 +44,17 @@ public class UserService {
                 .build();
 
         repository.save(user);
+    }
+
+    public TokenDTO login(LoginDTO dto) {
+        User user = repository.findByEmail(dto.email())
+                .orElseThrow(EmailNotFoundException::new);
+
+        if (!passwordEncoder.matches(dto.password(), user.getPassword())) {
+            throw new WrongPasswordException();
+        }
+        
+        return new TokenDTO(tokenService.generateToken(user));
     }
 
     private Authority getAuthority(String authority) {
