bf39eb9ba3a50be68ff1e9ed29a9e6204be2dced
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-10 07:20:04 -0300

    implement mapper for FullUserDTO with auxiliary method for authorities

--- src/main/java/com/martingarrote/beauty_salon_scheduler/mapper/UserMapper.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/mapper/UserMapper.java
@@ -1,11 +1,18 @@
 package com.martingarrote.beauty_salon_scheduler.mapper;
 
+import com.martingarrote.beauty_salon_scheduler.authority.Authority;
 import com.martingarrote.beauty_salon_scheduler.user.User;
 import com.martingarrote.beauty_salon_scheduler.user.dto.EmployeeDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.FullUserDTO;
 import com.martingarrote.beauty_salon_scheduler.user.dto.ProfileDTO;
 import com.martingarrote.beauty_salon_scheduler.user.dto.UserDTO;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Mapper
 public interface UserMapper {
@@ -15,4 +22,17 @@ public interface UserMapper {
     User toEntity(UserDTO userDTO);
     ProfileDTO toProfileDTO(User user);
     EmployeeDTO toEmployeeDTO(User user);
+    @Mapping(target = "authorities", source = "authorities", qualifiedByName = "authoritiesToStringSet")
+    FullUserDTO toFullUserDTO(User user);
+
+    @Named("authoritiesToStringSet")
+    default Set<String> authoritiesToStringSet(Set<Authority> authorities) {
+        if (authorities == null) {
+            return null;
+        }
+
+        return authorities.stream()
+                .map(Authority::getAuthority)
+                .collect(Collectors.toSet());
+    }
 }
