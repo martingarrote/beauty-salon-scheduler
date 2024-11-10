1356af5eca169de4858858c990c0be97121fd2ce
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-09 23:50:38 -0300

    implement UserMapper

---
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.martingarrote.beauty_salon_scheduler.mapper;
+
+import com.martingarrote.beauty_salon_scheduler.user.User;
+import com.martingarrote.beauty_salon_scheduler.user.dto.EmployeeDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.UserDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDTO toDTO(User user);
+    User toEntity(UserDTO userDTO);
+
+    EmployeeDTO toEmployeeDTO(User user);
+}
