3504d8f7a1ebcd07e0a4dbe98917f1ebd79ea088
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-10 16:05:52 -0300

    implement signup and login methods

--- src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserController.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/user/UserController.java
@@ -1,7 +1,14 @@
 package com.martingarrote.beauty_salon_scheduler.user;
 
+import com.martingarrote.beauty_salon_scheduler.user.dto.LoginDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.SignupDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.TokenDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService service;
+
+    @PostMapping("/signup")
+    public ResponseEntity<Void> signup(@RequestBody SignupDTO dto) {
+        service.signup(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginDTO dto) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.login(dto));
+    }
 
     private Long getUserId(Authentication authentication) {
         if (authentication.getPrincipal() instanceof User user) {
