163df7bbc0a30798507f550736cfc82252dbf722
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-10 19:16:29 -0300

    fix types in AppointmentDTO

--- src/main/java/com/martingarrote/beauty_salon_scheduler/appointment/dto/AppointmentDTO.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/appointment/dto/AppointmentDTO.java
@@ -1,7 +1,10 @@
 package com.martingarrote.beauty_salon_scheduler.appointment.dto;
 
 import com.martingarrote.beauty_salon_scheduler.beautyitem.BeautyItem;
+import com.martingarrote.beauty_salon_scheduler.beautyitem.dto.BeautyItemDTO;
 import com.martingarrote.beauty_salon_scheduler.user.User;
+import com.martingarrote.beauty_salon_scheduler.user.dto.EmployeeDTO;
+import com.martingarrote.beauty_salon_scheduler.user.dto.UserDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,11 +12,11 @@ import java.util.List;
 public record AppointmentDTO(
         LocalDateTime dateTime,
 
-        User employee,
+        EmployeeDTO employee,
 
-        User customer,
+        UserDTO customer,
 
-        List<BeautyItem> beautyItems,
+        List<BeautyItemDTO> beautyItems,
 
         String observations,
 
