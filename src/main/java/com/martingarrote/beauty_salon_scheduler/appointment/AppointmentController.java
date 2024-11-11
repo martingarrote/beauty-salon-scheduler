1252845d144f4f51690c65f560eca7a9911ffc70
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-11 03:56:49 -0300

    implement endpoints for appointment history and upcoming appointments

--- src/main/java/com/martingarrote/beauty_salon_scheduler/appointment/AppointmentController.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/appointment/AppointmentController.java
@@ -6,10 +6,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.martingarrote.beauty_salon_scheduler.utils.AuthenticationUtils.getUserId;
 
@@ -22,8 +21,17 @@ public class AppointmentController {
 
     @PostMapping
     public ResponseEntity<AppointmentDTO> create(@RequestBody CreationAppointmentDTO dto, Authentication auth) {
-        System.out.println(getUserId(auth));
         return ResponseEntity.status(HttpStatus.CREATED).body(service.create(dto, getUserId(auth)));
+    }
+
+    @GetMapping("/history")
+    public ResponseEntity<List<AppointmentDTO>> getAppointmentHistory(Authentication auth) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getAppointmentHistory(getUserId(auth)));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AppointmentDTO>> getNextAppointments(Authentication auth) {
+        return ResponseEntity.status(HttpStatus.OK).body(service.getNextAppointments(getUserId(auth)));
     }
 
 }
