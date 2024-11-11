54d3a6552c79345fc2644801c7be09b3b97c3f69
martingarrote <martindambrowskigarrote@gmail.com>
2024-11-11 04:20:42 -0300

    implement handlers for AppointmentNotFoundException and UnauthorizedAccessException

--- src/main/java/com/martingarrote/beauty_salon_scheduler/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/martingarrote/beauty_salon_scheduler/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.martingarrote.beauty_salon_scheduler.exceptions;
 
+import com.martingarrote.beauty_salon_scheduler.exceptions.appointment.AppointmentNotFoundException;
 import com.martingarrote.beauty_salon_scheduler.exceptions.appointment.AppointmentOverlapException;
 import com.martingarrote.beauty_salon_scheduler.exceptions.beautyitem.BeautyItemNotFoundException;
+import com.martingarrote.beauty_salon_scheduler.exceptions.common.UnauthorizedAccessException;
 import com.martingarrote.beauty_salon_scheduler.exceptions.user.*;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
@@ -61,9 +63,23 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(response.getStatus()).body(response);
     }
 
+    @ExceptionHandler(UnauthorizedAccessException.class)
+    private ResponseEntity<ExceptionResponse> unauthorizedAccessExceptionHandler(UnauthorizedAccessException exception) {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.UNAUTHORIZED, exception.getLocalizedMessage());
+
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
     @ExceptionHandler(AppointmentOverlapException.class)
     private ResponseEntity<ExceptionResponse> appointmentOverlapExceptionHandler(AppointmentOverlapException exception) {
         ExceptionResponse response = new ExceptionResponse(HttpStatus.CONFLICT, exception.getLocalizedMessage());
+
+        return ResponseEntity.status(response.getStatus()).body(response);
+    }
+
+    @ExceptionHandler(AppointmentNotFoundException.class)
+    private ResponseEntity<ExceptionResponse> appointmentNotFoundExceptionHandler(AppointmentNotFoundException exception) {
+        ExceptionResponse response = new ExceptionResponse(HttpStatus.NOT_FOUND, exception.getLocalizedMessage());
 
         return ResponseEntity.status(response.getStatus()).body(response);
     }
